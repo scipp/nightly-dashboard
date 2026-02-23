4bd521c7cea5e9e0c6830445172382f98dc2c72d
Neil Vaytet <neil.vaytet@ess.eu>
2026-02-23 14:24:53 +0100

    make red colors darker for tests that have been failing for a while

--- render.py
+++ render.py
@@ -404,6 +404,14 @@ def main(build_type, npipelines):
             message.write(content)
             logging.info(f"... wrote {filename}")
 
+    # Look at the test history: if the test has been failing more the last 5 times or more, change status to failed-long
+    for name, history in tests_history.items():
+        if history["status"][0] == "failed":
+            if len(history["status"]) >= 5 and all(
+                status == "failed" for status in history["status"][:5]
+            ):
+                all_tests[name][0].status = "failed-long"
+
     table_map = {group: {} for group in GROUPS}
     for name, history in all_tests.items():
         test = history[0]  # Use the first test as representative
